8383b3fbb27c0b78d3775876fd7672b70cfc2e4d
Pablo Firulo <pfirulo@yahoo.com>
2024-03-11 11:39:48 -0300

    cli pronto

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 
 	task "github.com/pfirulo2022/go-cli-crud.git/tasks"
 )
@@ -43,12 +46,49 @@ func main() {
 
 	if len(os.Args) < 2 {
 		printUsage()
+		return
+	} else {
+		switch os.Args[1] {
+		case "list":
+			task.ListTask(tasks)
+		case "add":
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Println("\n¿Cual es tu tarea?")
+			name, _ := reader.ReadString('\n')
+			name = strings.TrimSpace(name)
+
+			tasks = task.AddTask(tasks, name)
+			task.SaveTask(file, tasks)
+
+		case "delete":
+			if len(os.Args) < 3 {
+				fmt.Println("Debes proporcionar un ID.")
+			}
+			id, err := strconv.Atoi(os.Args[2])
+			if err != nil {
+				fmt.Println("El id debe ser un número.")
+			}
+
+			tasks = task.DeleteTask(tasks, id)
+			task.SaveTask(file, tasks)
+
+		case "complete":
+			if len(os.Args) < 3 {
+				fmt.Println("Debes proporcionar un ID.")
+
+			}
+			id, err := strconv.Atoi(os.Args[2])
+			if err != nil {
+				fmt.Println("El id debe ser un número.")
+
+			}
+			tasks = task.CompleteTask(tasks, id)
+			task.SaveTask(file, tasks)
+
+		}
+
 	}
 
-	switch os.Args[1] {
-	case "list":
-		task.ListTask(tasks)
-	}
 }
 
 func printUsage() {
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,6 +1,11 @@
 package tasks
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type Task struct {
 	ID       int    `json:"id"`
@@ -14,7 +19,74 @@ func ListTask(tasks []Task) {
 		return
 	}
 
-	for i, task := range tasks {
-		fmt.Println("%d %s", i, task.Name)
+	for _, task := range tasks {
+		status := "  "
+		if task.Complete {
+			status = "✔ "
+		}
+		fmt.Printf("[%s] %d %s\n", status, task.ID, task.Name)
 	}
+}
+
+func AddTask(tasks []Task, name string) []Task {
+	newTask := Task{
+		ID:       GetNextID(tasks),
+		Name:     name,
+		Complete: false,
+	}
+	return append(tasks, newTask)
+}
+
+func SaveTask(file *os.File, tasks []Task) {
+	bytes, err := json.Marshal(tasks)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		panic(err)
+	}
+	err = file.Truncate(1)
+	if err != nil {
+		panic(err)
+	}
+
+	writer := bufio.NewWriter(file)
+	_, err = writer.Write(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func DeleteTask(tasks []Task, id int) []Task {
+	for i, task := range tasks {
+		if task.ID == id {
+			return append(tasks[:i], tasks[i+1:]...)
+		}
+	}
+	return tasks
+}
+
+func GetNextID(tasks []Task) int {
+	if len(tasks) == 0 {
+		return 1
+	}
+	return tasks[len(tasks)-1].ID + 1
+}
+
+func CompleteTask(tasks []Task, id int) []Task {
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks[i].Complete = true
+			break
+		}
+	}
+	return tasks
 }
